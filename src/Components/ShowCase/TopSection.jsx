36706e25c0bd35537e566d6ae99324ab00301d9b
Yunus AbdulMajid <Yunusabdulmajidyunus38@gmail.com>
2023-09-08 23:59:13 +0100

    new push update

--- src/Components/ShowCase/TopSection.jsx
+++ src/Components/ShowCase/TopSection.jsx
@@ -57,13 +57,45 @@ function TopSection() {
         </div>
         <Testimony />
       </section>
-      <section className=" px-5 pt-20 pb-10 md:px-24 md:py-4 bg-girls bg-cover">
-      <h1>Get Help, Say Hello</h1>
-        <p>
+      <section className="bg-black text-[#fff] grid gap-4 md:gap-8 px-5 pt-20 pb-10 md:px-24  bg-girls bg-cover bg-center bg-no-repeat md:py-20">
+      <h1 className="text-3xl font-medium">Get Help, Say Hello</h1>
+        <p className="md:w-[35%]">
           Get expert help, advice and tips in-app. Connect with us on our social
-          channels and join the
+          channels and join the <br/>
+          #ChipperCash community online.
         </p>
-        <p>#ChipperCash community online.</p>
+        <div className="md:grid-cols-6 grid grid-cols-3 gap-6 w-2/3 md:w-[35%]">
+            <img
+            src="https://global-uploads.webflow.com/63c81b0c3ad929013f062d70/647084316ca1612c47df448e_Vector.svg"
+            alt="twitter"
+            className="w-10 grayscale hover:grayscale-0 transition duration-200 ease-out"
+          />
+          <img
+            src="https://global-uploads.webflow.com/63c81b0c3ad929013f062d70/647083e36ca1612c47df069a_instagram%201.svg"
+            alt="instagram"
+            className="w-10 grayscale hover:grayscale-0 transition duration-200 ease-out"
+          />
+          <img
+            src="https://global-uploads.webflow.com/63c81b0c3ad929013f062d70/64708431a61024d9b309b970_Vector-3.svg"
+            alt="facebook"
+            className="w-10 grayscale hover:grayscale-0 transition duration-200 ease-out"
+          />
+          <img
+            src="https://global-uploads.webflow.com/63c81b0c3ad929013f062d70/64708431deace073b84f0663_Vector-2.svg"
+            alt="youtube"
+            className="w-10 grayscale hover:grayscale-0 transition duration-200 ease-out"
+          />
+          <img
+            src="https://global-uploads.webflow.com/63c81b0c3ad929013f062d70/647084856201b89f5f440abf_Isolation_Mode.svg"
+            alt="tiktok"
+            className="w-10 grayscale hover:grayscale-0 transition duration-200 ease-out"
+          />
+          <img
+            src="https://global-uploads.webflow.com/63c81b0c3ad929013f062d70/647084313f6ba1683123aee4_Group-4.svg"
+            alt="linkdlin"
+            className="w-10 grayscale hover:grayscale-0 transition duration-200 ease-out"
+          />
+        </div>
       </section>
     </>
   );
